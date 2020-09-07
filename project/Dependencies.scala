494eab5b71dba76d44a49b43871cd57a973c9dc2
pawelkaczor <newion@o2.pl>
2020-09-07 16:09:52 +0200

    update fastparse to 2.2.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   val logback       = "ch.qos.logback" % "logback-classic" % "1.1.3"
   val scalaCompiler = "org.scala-lang" % "scala-compiler"  % "2.13.3"
   val scalaReflect  = "org.scala-lang" % "scala-reflect"   % "2.13.3"
-  val fastParse     = "com.lihaoyi"   %% "fastparse"       % "2.2.0"
+  val fastParse     = "com.lihaoyi"   %% "fastparse"       % "2.2.2"
 
   // ------------------- test
   val scalaTest = "org.scalatest" %% "scalatest" % "3.2.0" % Test
