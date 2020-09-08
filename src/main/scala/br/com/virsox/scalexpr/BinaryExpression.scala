e4c5d3a1add9de8ec5fcf61803cd1a81c7ee91f6
pawelkaczor <newion@o2.pl>
2020-09-08 10:04:38 +0200

    minor, reformat

--- src/main/scala/br/com/virsox/scalexpr/BinaryExpression.scala
+++ src/main/scala/br/com/virsox/scalexpr/BinaryExpression.scala
@@ -5,7 +5,7 @@ import br.com.virsox.scalexpr.Context.{ContextLike, EmptyContext}
 import scala.math.Ordering
 import scala.reflect.runtime.universe._
 
-/***
+/** *
   * Trait for binary expressions.
   * @tparam A Type of the left operand.
   * @tparam B Type of the right operand.
@@ -35,25 +35,26 @@ object GreaterEqualsThan extends RelationalOperator
   * @tparam T Type of the operands.
   */
 case class RelationalExpression[T: Ordering](left: Expression[T], op: RelationalOperator, right: Expression[T])
-  extends BooleanExpr
-  with BinaryExpression[T, T, Boolean] {
+    extends BooleanExpr
+    with BinaryExpression[T, T, Boolean] {
 
   /** Ordering object. */
-  val ordering = implicitly[Ordering[T]]
+  val ordering: Ordering[T] = implicitly[Ordering[T]]
 
   /**
     * Resolves the relational expression.
     * @param context Not used directly by the expression, but it is passed recursively to resolve the operands.
     * @return Value of the expression (always a boolean).
     */
-  override def resolve[A](context: A = EmptyContext)(implicit ctxLike: ContextLike[A]): Boolean = op match {
-    case Equals            => left.resolve(context) == right.resolve(context)
-    case NotEquals         => left.resolve(context) != right.resolve(context)
-    case LessThan          => ordering.lt(left.resolve(context), right.resolve(context))
-    case LessEqualsThan    => ordering.lteq(left.resolve(context), right.resolve(context))
-    case GreaterThan       => ordering.gt(left.resolve(context), right.resolve(context))
-    case GreaterEqualsThan => ordering.gteq(left.resolve(context), right.resolve(context))
-  }
+  override def resolve[A](context: A = EmptyContext)(implicit ctxLike: ContextLike[A]): Boolean =
+    op match {
+      case Equals            => left.resolve(context) == right.resolve(context)
+      case NotEquals         => left.resolve(context) != right.resolve(context)
+      case LessThan          => ordering.lt(left.resolve(context), right.resolve(context))
+      case LessEqualsThan    => ordering.lteq(left.resolve(context), right.resolve(context))
+      case GreaterThan       => ordering.gt(left.resolve(context), right.resolve(context))
+      case GreaterEqualsThan => ordering.gteq(left.resolve(context), right.resolve(context))
+    }
 }
 
 // ---------------------------------------------------------------------------------------------------------
@@ -70,30 +71,31 @@ object Or  extends LogicalOperator
   * @param right Right operand.
   */
 case class LogicalExpression(left: Expression[Boolean], op: LogicalOperator, right: Expression[Boolean])
-  extends BooleanExpr
-  with BinaryExpression[Boolean, Boolean, Boolean] {
+    extends BooleanExpr
+    with BinaryExpression[Boolean, Boolean, Boolean] {
 
   /**
     * Resolves the logical operation.
     * @param context Not used directly by the expression, but it is passed recursively to resolve the operands.
     * @return Value of the expression.
     */
-  override def resolve[A](context: A = EmptyContext)(implicit ctxLike: ContextLike[A]): Boolean = op match {
-    case And => left.resolve(context) && right.resolve(context)
-    case Or  => left.resolve(context) || right.resolve(context)
-  }
+  override def resolve[A](context: A = EmptyContext)(implicit ctxLike: ContextLike[A]): Boolean =
+    op match {
+      case And => left.resolve(context) && right.resolve(context)
+      case Or  => left.resolve(context) || right.resolve(context)
+    }
 }
 
 // ---------------------------------------------------------------------------------------------------------
 
 /** Arithmetic operators. */
 class ArithmeticOperator
-object Plus extends ArithmeticOperator
+object Plus  extends ArithmeticOperator
 object Minus extends ArithmeticOperator
 object Times extends ArithmeticOperator
 object Div   extends ArithmeticOperator
 
-/***
+/** *
   * Arithmetic operation. Both operands must be of the same type. Expression results in the same type.
   * @param left Left operand.
   * @param op Arithmetic operation.
@@ -101,32 +103,30 @@ object Div   extends ArithmeticOperator
   * @tparam T Type of the operands and return type.
   */
 case class ArithmeticExpression[T: Numeric: TypeTag](left: Expression[T], op: ArithmeticOperator, right: Expression[T])
-  extends NumericExpr[T]
-  with BinaryExpression[T, T, T] {
+    extends NumericExpr[T]
+    with BinaryExpression[T, T, T] {
 
   /**
     * Resolves the arithmetic operation.
     * @param context Not used directly by the expression, but it is passed recursively to resolve the operands.
     * @return Value of the expression.
     */
-  override def resolve[A](context: A = EmptyContext)(implicit ctxLike: ContextLike[A]): T = op match {
-    case Plus      => numericEvidence.plus(left.resolve(context), right.resolve(context))
-    case Minus     => numericEvidence.minus(left.resolve(context), right.resolve(context))
-    case Times     => numericEvidence.times(left.resolve(context), right.resolve(context))
-    case Div => {
-
-      // division is not defined in the Numeric type
-      if (typeOf[T] =:= typeOf[Double]) {
-        (numericEvidence.toDouble(left.resolve(context)) / numericEvidence.toDouble(right.resolve(context))).asInstanceOf[T]
-      } else if (typeOf[T] =:= typeOf[Float]) {
-        (numericEvidence.toFloat(left.resolve(context)) / numericEvidence.toFloat(right.resolve(context))).asInstanceOf[T]
-      } else if (typeOf[T] =:= typeOf[Long]) {
-        (numericEvidence.toLong(left.resolve(context)) / numericEvidence.toLong(right.resolve(context))).asInstanceOf[T]
-      } else {
-        (numericEvidence.toInt(left.resolve(context)) / numericEvidence.toInt(right.resolve(context))).asInstanceOf[T]
-      }
+  override def resolve[A](context: A = EmptyContext)(implicit ctxLike: ContextLike[A]): T =
+    op match {
+      case Plus  => numericEvidence.plus(left.resolve(context), right.resolve(context))
+      case Minus => numericEvidence.minus(left.resolve(context), right.resolve(context))
+      case Times => numericEvidence.times(left.resolve(context), right.resolve(context))
+      case Div   =>
+        // division is not defined in the Numeric type
+        if (typeOf[T] =:= typeOf[Double]) {
+          (numericEvidence.toDouble(left.resolve(context)) / numericEvidence.toDouble(right.resolve(context))).asInstanceOf[T]
+        } else if (typeOf[T] =:= typeOf[Float]) {
+          (numericEvidence.toFloat(left.resolve(context)) / numericEvidence.toFloat(right.resolve(context))).asInstanceOf[T]
+        } else if (typeOf[T] =:= typeOf[Long]) {
+          (numericEvidence.toLong(left.resolve(context)) / numericEvidence.toLong(right.resolve(context))).asInstanceOf[T]
+        } else {
+          (numericEvidence.toInt(left.resolve(context)) / numericEvidence.toInt(right.resolve(context))).asInstanceOf[T]
+        }
     }
-  }
 
 }
-
--- src/main/scala/br/com/virsox/scalexpr/SimpleExpression.scala
+++ src/main/scala/br/com/virsox/scalexpr/SimpleExpression.scala
@@ -7,12 +7,11 @@ import br.com.virsox.scalexpr.Context.{ContextLike, EmptyContext}
 /** Simple / unary expressions. */
 trait SimpleExpression[T] extends Expression[T]
 
-
-/**  Constant expression */
+/** Constant expression */
 trait Constant[T] extends SimpleExpression[T] {
   def value: T
 
-  /***
+  /** *
     * Simply returns the constant value.
     * @param context Not used.
     * @return Value of the constant.
@@ -20,13 +19,12 @@ trait Constant[T] extends SimpleExpression[T] {
   override def resolve[A](context: A = EmptyContext)(implicit ctxLike: ContextLike[A]): T = value
 }
 
-case class BooleanConstant(value: Boolean) extends BooleanExpr with Constant[Boolean]
-case class IntConstant(value: Int) extends NumericExpr[Int] with Constant[Int]
-case class LongConstant(value: Long) extends NumericExpr[Long] with Constant[Long]
-case class DoubleConstant(value: Double) extends NumericExpr[Double] with Constant[Double]
-case class StringConstant(value: String) extends OrderingExpr[String] with Constant[String]
+case class BooleanConstant(value: Boolean)  extends BooleanExpr with Constant[Boolean]
+case class IntConstant(value: Int)          extends NumericExpr[Int] with Constant[Int]
+case class LongConstant(value: Long)        extends NumericExpr[Long] with Constant[Long]
+case class DoubleConstant(value: Double)    extends NumericExpr[Double] with Constant[Double]
+case class StringConstant(value: String)    extends OrderingExpr[String] with Constant[String]
 case class DateTimeConstant(value: Instant) extends OrderingExpr[Instant] with Constant[Instant]
-
 
 /** Represents a variable. */
 trait Variable[T] extends SimpleExpression[T] {
@@ -42,26 +40,26 @@ trait Variable[T] extends SimpleExpression[T] {
     * @return Value of the variable.
     */
   override def resolve[A](context: A = EmptyContext)(implicit ctxLike: ContextLike[A]): T = {
-    val tokens = name.split("\\.")
+    val tokens                  = name.split("\\.")
     val (eventNames, attribute) = tokens.splitAt(tokens.length - 1)
 
-    val lastEventOption = eventNames.foldLeft(Some(context): Option[A])(
-      (ctx, field) => ctx.flatMap((c) => ctxLike.nested(c, field))
-    )
+    val lastEventOption =
+      eventNames.foldLeft(Some(context): Option[A])((ctx, field) => ctx.flatMap(c => ctxLike.nested(c, field)))
 
     lastEventOption match {
-      case Some(ctx) => ctxLike.get[T](ctx, attribute(0)) match {
-        case Some(value) => value
-        case None        => throw new IllegalArgumentException("variable not found in context")
-      }
+      case Some(ctx) =>
+        ctxLike.get[T](ctx, attribute(0)) match {
+          case Some(value) => value
+          case None        => throw new IllegalArgumentException("variable not found in context")
+        }
       case None => throw new IllegalArgumentException("variable not found in context")
     }
   }
 }
 
-case class BooleanVar(name: String) extends BooleanExpr with Variable[Boolean]
-case class IntVar(name: String) extends NumericExpr[Int] with Variable[Int]
-case class LongVar(name: String) extends NumericExpr[Long] with Variable[Long]
-case class DoubleVar(name: String) extends NumericExpr[Double] with Variable[Double]
-case class StringVar(name: String) extends OrderingExpr[String] with Variable[String]
+case class BooleanVar(name: String)  extends BooleanExpr with Variable[Boolean]
+case class IntVar(name: String)      extends NumericExpr[Int] with Variable[Int]
+case class LongVar(name: String)     extends NumericExpr[Long] with Variable[Long]
+case class DoubleVar(name: String)   extends NumericExpr[Double] with Variable[Double]
+case class StringVar(name: String)   extends OrderingExpr[String] with Variable[String]
 case class DateTimeVar(name: String) extends OrderingExpr[Instant] with Variable[Instant]
